f9c69bc89cf5b7112ff9daa5839bd5b868f4f107
centonhuang <centonhuang@tencent.com>
2026-03-05 19:19:31 +0800

    ✨ feat: add deployment scripts for development and production environments
    - Created deploy-dev.sh for setting up the development environment with Docker.
    - Created deploy.sh for deploying the production environment with Docker.
    - Both scripts include pulling the latest Docker image, starting services, pruning unused images, and displaying logs.

---
+++ script/deploy.sh
@@ -0,0 +1,13 @@
+echo -e "\033[1;36mDeploying production environment...\033[0m"
+
+echo -e "\033[1;32mPulling the latest Docker image...\033[0m"
+docker pull ghcr.io/hcd233/aris-proxy-api:master
+
+echo -e "\033[1;34mStarting up services with docker-compose...\033[0m"
+docker compose -f docker/docker-compose-single.yml up -d
+
+echo -e "\033[1;31mPruning unused Docker images...\033[0m"
+docker image prune -a -f
+
+echo -e "\033[1;33mDisplaying Docker logs for aris-proxy-api...\033[0m"
+docker logs -f aris-proxy-api --details
