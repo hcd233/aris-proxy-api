0cc0953ca9c4c0c4e210e47bb5d393ec44b5c19b
centonhuang <centonhuang@tencent.com>
2026-05-26 15:53:57 +0800

    fix(web): replace static middleware with direct embed.FS serving to fix page crash

--- internal/router/web.go
+++ internal/router/web.go
@@ -2,12 +2,27 @@ package router
 
 import (
 	"io/fs"
+	"mime"
+	pathpkg "path"
+	"path/filepath"
+	"strings"
 
 	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/static"
 	"github.com/hcd233/aris-proxy-api/internal/logger"
 	"go.uber.org/zap"
 )
+
+var commonMIME = map[string]string{
+	".js":    "application/javascript",
+	".css":   "text/css",
+	".woff2": "font/woff2",
+	".woff":  "font/woff",
+	".ico":   "image/x-icon",
+	".svg":   "image/svg+xml",
+	".png":   "image/png",
+	".json":  "application/json",
+	".txt":   "text/plain",
+}
 
 func RegisterWebRouter(app *fiber.App, webFS fs.FS) {
 	subFS, err := fs.Sub(webFS, "dist")
@@ -22,12 +37,25 @@ func RegisterWebRouter(app *fiber.App, webFS fs.FS) {
 		return
 	}
 
-	app.Use("/web", static.New("", static.Config{
-		FS:         subFS,
-		IndexNames: []string{"index.html"},
-	}))
-
 	app.Get("/web/*", func(c fiber.Ctx) error {
+		filePath := strings.TrimPrefix(c.Path(), "/web/")
+		filePath = strings.TrimPrefix(filePath, "/")
+
+		if filePath == "" || strings.HasSuffix(filePath, "/") {
+			filePath = pathpkg.Join(filePath, "index.html")
+		}
+
+		data, err := fs.ReadFile(subFS, filePath)
+		if err == nil {
+			ext := filepath.Ext(filePath)
+			if ct, ok := commonMIME[ext]; ok {
+				c.Type(ct)
+			} else if ct := mime.TypeByExtension(ext); ct != "" {
+				c.Type(ct)
+			}
+			return c.Send(data)
+		}
+
 		c.Type("html", "utf-8")
 		return c.Send(indexContent)
 	})
