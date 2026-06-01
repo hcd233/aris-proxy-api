a2c7e03485c0af2e28ea87431cdc338c98b61153
centonhuang <centonhuang@tencent.com>
2026-06-01 19:46:44 +0800

    feat(lock): add Refresh method with owner-only PEXPIRE

--- internal/lock/lock.go
+++ internal/lock/lock.go
@@ -9,14 +9,11 @@ import (
 
 // Locker 锁接口
 //
-//	@param ctx context.Context
-//	@param key string
-//	@param value string
-//	@return err error
 //	@author centonhuang
-//	@update 2025-11-11 16:54:41
+//	@update 2026-06-01 10:00:00
 type Locker interface {
 	Lock(ctx context.Context, key string, value string, expire time.Duration) (success bool, err error)
+	Refresh(ctx context.Context, key string, value string, expire time.Duration) (success bool, err error)
 	Unlock(ctx context.Context, key string, value string) (err error)
 }
 
@@ -24,7 +21,7 @@ type Locker interface {
 //
 //	@return Locker
 //	@author centonhuang
-//	@update 2025-11-11 17:49:18
+//	@update 2026-06-01 10:00:00
 func NewLocker(cache *redis.Client) Locker {
 	return &redisLocker{cache: cache}
 }
@@ -33,17 +30,34 @@ type redisLocker struct {
 	cache *redis.Client
 }
 
+const luaRefresh = `
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+    return redis.call("PEXPIRE", KEYS[1], ARGV[2])
+else
+    return 0
+end
+`
+
+const luaUnlock = `
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+    return redis.call("DEL", KEYS[1])
+else
+    return 0
+end
+`
+
 func (l *redisLocker) Lock(ctx context.Context, key, value string, expire time.Duration) (success bool, err error) {
 	return l.cache.SetNX(ctx, key, value, expire).Result()
 }
 
+func (l *redisLocker) Refresh(ctx context.Context, key, value string, expire time.Duration) (success bool, err error) {
+	res, err := l.cache.Eval(ctx, luaRefresh, []string{key}, value, expire.Milliseconds()).Int64()
+	if err != nil {
+		return false, err
+	}
+	return res == 1, nil
+}
+
 func (l *redisLocker) Unlock(ctx context.Context, key, value string) (err error) {
-	luaScript := `
-			if redis.call("get", KEYS[1]) == ARGV[1] then
-				return redis.call("del", KEYS[1])
-			else
-				return 0
-			end
-		`
-	return l.cache.Eval(ctx, luaScript, []string{key}, value).Err()
+	return l.cache.Eval(ctx, luaUnlock, []string{key}, value).Err()
 }
