3927d4c8f4f70cb1b3752e99f6051a1961c96aa4
centonhuang <centonhuang@tencent.com>
2026-06-04 00:41:20 +0800

    chore(cron): change think extract schedule from midnight to 1 AM

--- internal/common/constant/cron.go
+++ internal/common/constant/cron.go
@@ -8,7 +8,7 @@ const (
 	CronModuleSoftDeletePurge = "SoftDeletePurgeCron"
 	CronSpecSoftDeletePurge   = "0 4 * * 0"
 	CronModuleThinkExtract    = "ThinkExtractCron"
-	CronSpecThinkExtract      = "0 0 * * *"
+	CronSpecThinkExtract      = "0 1 * * *"
 
 	// CronLockDefaultTTL 默认 cron 任务分布式锁 TTL
 	CronLockDefaultTTL = 5 * time.Minute
