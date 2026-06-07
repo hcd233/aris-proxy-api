11e4602f0b87781d469b766164ebaf08a8e6b842
centonhuang <centonhuang@tencent.com>
2026-06-07 20:43:47 +0800

    Revert "perf(session): add pg_trgm + GIN indexes for keyword search on /api/v1/session/list"
    This reverts commit 75658e53270ca83ef8bacc9e90d655952267efac.

--- cmd/database.go
+++ cmd/database.go
@@ -18,7 +18,6 @@ var migrateDatabaseCmd = &cobra.Command{
 	Long:  `Execute database migration operation, update the database structure to the latest mode.`,
 	Run: func(cmd *cobra.Command, _ []string) {
 		lo.Must0(database.AutoMigrate(cmd.Context()))
-		lo.Must0(database.EnsureSearchIndexes(cmd.Context()))
 	},
 }
 
